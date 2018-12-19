39f809a5a53d7eae1446c7c2244152af2efc56ec
FootstepWorks <lheesoo89@gmail.com>
2018-12-19 17:51:20 +0900

    refactoring

--- src/main/java/com/ironshutter/web/domain/model/account/Account.java
+++ src/main/java/com/ironshutter/web/domain/model/account/Account.java
@@ -2,18 +2,20 @@ package com.ironshutter.web.domain.model.account;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
-
-import com.ironshutter.web.interfaces.sign.facade.dto.SignUpForm;
 
 
 @Entity
@@ -31,10 +33,13 @@ public class Account implements Serializable{
 	@Type(type="org.hibernate.type.NumericBooleanType")
 	private Boolean isRetired = false;
 	
-	@OneToOne(mappedBy="account", fetch = FetchType.LAZY)
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
 	private User user;
 	
-	@OneToOne(mappedBy="account", fetch = FetchType.LAZY)
+	@OneToOne(cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
 	private Sign sign;
 
 	public Account() {}
--- src/main/java/com/ironshutter/web/domain/model/account/Sign.java
+++ src/main/java/com/ironshutter/web/domain/model/account/Sign.java
@@ -1,10 +1,12 @@
 package com.ironshutter.web.domain.model.account;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -22,8 +24,9 @@ public class Sign {
 	@Column(name="hashed_password")
 	private String hashedPassword;
 	
-	@OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account_id")
+	@MapsId
+	@OneToOne(mappedBy="sign", fetch = FetchType.LAZY)
+	@JoinColumn(name="account_id")
 	private Account account;
 	
 	public Sign() {}
